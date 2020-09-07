bb5a1f19a498c500370869c0e68bc15fcb864cd9
danielmpaez <53493045+danielmpaez@users.noreply.github.com>
2020-09-07 18:08:23 -0300

    Update players.py

--- players.py
+++ players.py
@@ -1,54 +1,33 @@
-from flask import Flask,jsonify, render_template,request,redirect,url_for # For flask implementation    
-from pymongo import MongoClient    
-import os  
-import json  
-from bson import json_util, ObjectId 
-    
-app = Flask(__name__)       
- 
-Mongoclient = 'mongodb://ababab:xzxzxz@3.tcp.ngrok.io:23370/scpipeline?authSource=admin'  #host
-#MONGO_PORT = 23370  #port
-#MONGO_DB = 'scpipeline' #select database'
-#MONGO_USER = 'ababab'  #username
-#MONGO_PASS = 'xzxzxz'    #password
+  from flask import Flask, request, render_template
+import os
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import bson
+import jsonify
+import json
 
-
-#conection
+Mongoclient= 'mongodb://starcraft:testing@3.tcp.ngrok.io:23370/scpipeline?authSource=admin'  #host
 client = MongoClient(os.getenv("MONGO_URI"))
-db = client['users']
+db = client['scpipeline']
 
 
-print(db)   
-#collection
-players = db.new_replays #Select the collection name     
-print(players)
+app = Flask(__name__)
 
+@app.route('/list', methods=['GET'])
+def cast_index():
+  ret = list(db.new_replays.find({}))
+  json_data = json.dumps(ret)
+  return json_data
 
-cursor = client.list_databases()
-for db in cursor:
-    print(db)
+  for x in ret.find():
+    print(x)
 
-#route   
-@app.route("/list")    
-def get_replays():
-   
-    response = jsonify(players)
-    return Response(response)
-
-    print(response)
+@app.route('/uno')
+def html1():
+   return  render_template('index.html')
 
 
 
-
-
-
-
-    
-  
-
-  
-
-  
 if __name__ == "__main__":    
     
     app.run(debug=True)  
