546e5b31814442e0dd7868c04bed5e7a78ffb234
danielmpaez <53493045+danielmpaez@users.noreply.github.com>
2020-11-03 12:28:26 -0300

    Update youtube4.py

--- youtube4.py
+++ youtube4.py
@@ -1,7 +1,7 @@
 import json
 
 #from dateutil.parser import parse
-
+import  requests
 import http.client
 import os
 
@@ -10,10 +10,9 @@ YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
 YOUTUBE_ID = "LFoQqAI2LSo"
 
 youtube_client = http.client.HTTPSConnection("www.googleapis.com")
-'''GET https://www.googleapis.com/youtube/v3/search'''
-'''GET https://www.googleapis.com/youtube/v3/playlistItems'''
+
 #youtube_client.request("GET", "/youtube/v3/search?part=myRating,pageToken,statistics,status&channelId=UCZ7wN9kEDmRFhQ7MAotRIAQ&id={}&key={}".format(YOUTUBE_ID, YOUTUBE_API_KEY))
-youtube_client.request("GET","https://www.googleapis.com/youtube/v3/search?part=$args&channelId=UCZ7wN9kEDmRFhQ7MAotRIAQ&key=$&order=date&maxResults=10".format(YOUTUBE_ID, YOUTUBE_API_KEY))
+youtube_client.request("GET","/youtube/v3/search?part=id,snippet&channelId=UCZ7wN9kEDmRFhQ7MAotRIAQ&order=date&maxResults=10&key={}".format(YOUTUBE_API_KEY))
 
 
 response = youtube_client.getresponse()
@@ -24,6 +23,19 @@ if response.status != 200:
 
 data = json.loads(response.read())
 print(data)
+
+r = requests.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&channelId=UCZ7wN9kEDmRFhQ7MAotRIAQ&order=date&key={}".format(YOUTUBE_API_KEY))
+json_data = r.json()                                                                                                
+nextPageToken = json_data.get("CAoQAA")
+print(json_data)
+# Devuelve el resto de las paginas en un bucle
+while nextPageToken:
+    r = requests.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&channelId=UCZ7wN9kEDmRFhQ7MAotRIAQ&order=date&key={}&pageToken=CAoQAA".format(YOUTUBE_API_KEY))
+    json_data = r.json()
+    nextPageToken = json_data.get("CAoQAA")
+print(json_data)
+
+
 # por cada item guardar en db.videos:
 # - item['id']
 # - item['snippet']['title']
